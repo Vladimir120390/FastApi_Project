60b2aa4230f73df59a0c4f1ba5edf315f1df26ed
Vladimir120390 <166973041+Vladimir120390@users.noreply.github.com>
2025-01-02 14:24:59 +0400

    Add files via upload

---
+++ FasApi_Project/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.routers import task, user
+
+app = FastAPI()
+
+@app.get("/", tags=["welcome"])
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
+
+# Подключаем маршруты
+app.include_router(user.router)
+app.include_router(task.router)
+
+
---
+++ FasApi_Project/app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from app.backend.db import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True)
+
